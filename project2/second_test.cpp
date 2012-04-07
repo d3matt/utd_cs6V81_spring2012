d8c3884491418c96b47be3e03bbbc4e3f266d671
Matthew Stoltenberg <d3matt@gmail.com>
2012-04-07 09:24:50 -0500

    make second_test work
    see discussion on how we want to time things

--- project2/second_test.cpp
+++ project2/second_test.cpp
@@ -22,30 +22,6 @@
 
 using namespace std;
 
-static uint64_t counter;
-
-static void *worker_thread(void *Arg)
-{
-    worker_thread_arg * arg = (worker_thread_arg *) Arg;
-    printf("Start of thread %d\n", arg->tnum);
-    for(uint32_t i=0 ; i < 10000; i++) {
-        DBGDISP("thread %u lock()", arg->tnum);
-        arg->lock->lock();
-        DBGDISP("thread %u back", arg->tnum);
-        counter++;
-
-        //yield each time through loop to make things interesting...
-        pthread_yield();
-
-        DBGDISP("thread %u unlock()", arg->tnum);
-        arg->lock->unlock();
-        DBGDISP("thread %u back", arg->tnum);
-    }
-    printf("End of thread %d\n", arg->tnum);
-    delete arg;
-    return NULL;
-}
-
 bool ts_lt(timespec *a, timespec *b)
 {
     if(a->tv_sec > b->tv_sec)
@@ -57,33 +33,75 @@ bool ts_lt(timespec *a, timespec *b)
     return false;
 }
 
+static uint64_t counter;
+static uint64_t *tcounters;
 
-int main(int argc, char ** argv)
+static void *worker_thread(void *Arg)
 {
-    common_args         carg;
-
-    parse_args(argc, argv, &carg);
-
+    worker_thread_arg * arg = (worker_thread_arg *) Arg;
+    
+    //not sure if each thread should run for 10 seconds or overall we should run for 10 seconds...
     timespec start;
     timespec stop;
     timespec current;
     clock_gettime( CLOCK_REALTIME, &start );
-    cout << "Start : " << start.tv_sec << "." << start.tv_nsec << endl;
     current = stop = start;
-    stop.tv_sec+=10;
-    
-    while( ts_lt(&current, &stop) )
-    {
+    stop.tv_sec += arg->carg->num_seconds;
+    //printf("start thread %2d : %ld.%ld\n", arg->tnum, current.tv_sec, current.tv_nsec);
+
+    while( ts_lt(&current, &stop) ) {
+        DBGDISP("thread %u lock()", arg->tnum);
+        arg->lock->lock();
+        DBGDISP("thread %u back", arg->tnum);
         counter++;
+        tcounters[arg->tnum]++;
+
+        //yield each time through loop to make things interesting...
+        pthread_yield();
+
+        DBGDISP("thread %u unlock()", arg->tnum);
+        arg->lock->unlock();
+        DBGDISP("thread %u back", arg->tnum);
+
         clock_gettime( CLOCK_REALTIME, &current );
     }
-    cout << " Stop : " << current.tv_sec << "." << current.tv_nsec << endl;
+    //printf(" stop thread %2d : %ld.%ld %ld\n", arg->tnum, current.tv_sec, current.tv_nsec, arg->tcounter);
+    delete arg;
+    return NULL;
+}
 
-    return 0;
+int main(int argc, char ** argv)
+{
+    common_args carg;
+    int         retval=0;
+
+    parse_args(argc, argv, &carg);
+
+    timespec start;
+    timespec current;
+    clock_gettime( CLOCK_REALTIME, &start );
+
+    tcounters = new uint64_t[carg.num_threads];
 
     test_common(&carg, worker_thread);
 
+    clock_gettime( CLOCK_REALTIME, &current );
+
+    cout << "Test Start : " << start.tv_sec << "." << start.tv_nsec << endl;
+    cout << " Test Stop : " << current.tv_sec << "." << current.tv_nsec << endl;
+
+
     cout << "counter is: " << counter << endl;
+    uint64_t test=0;
+    for(uint32_t i=0; i < carg.num_threads; i++)
+    {
+        test += tcounters[i];
+    }
+    if(test != counter)
+        retval=1;
+
+    delete tcounters;
+    return retval;
 }
 
 
